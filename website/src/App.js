3e1bb7c9a7577a655bdb0d3fdf436f29c9fab5e6
Quinn Melssen <qjm7@nau.edu>
2022-01-08 16:44:35 -0700

    Adding default login page

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import SignUpForm from "./Components/SignUpForm";
+import SignInForm from "./Components/SignInForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <div className="appAside" />
+                    <div className="appForm">
+                        <div className="pageSwitcher">
+                            <NavLink
+                                to="/sign-in"
+                                activeClassName="pageSwitcherItem-active"
+                                className="pageSwitcherItem"
+                            >
+                                Sign In
+                            </NavLink>
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="pageSwitcherItem-active"
+                                className="pageSwitcherItem"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+
+                        <div className="formTitle">
+                            <NavLink
+                                to="/sign-in"
+                                activeClassName="formTitleLink-active"
+                                className="formTitleLink"
+                            >
+                                Sign In
+                            </NavLink>{" "}
+                            or{" "}
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="formTitleLink-active"
+                                className="formTitleLink"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+                        <Routes>
+                            <Route path="/sign-in" element={<SignInForm />}>
+                                Sign In
+                            </Route>
+                            <Route path="/" element={<SignUpForm />}>
+                                Sign Up
+                            </Route>
+                        </Routes>
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
