a5cede5c91c6abebcea447b5b85ffe4a79cf511b
Quinn Melssen <qjm7@nau.edu>
2022-02-11 21:17:48 -0700

    Accounting for reply edge case

--- website/server/email_scraper.py
+++ website/server/email_scraper.py
@@ -1,12 +1,16 @@
-# imports
+# IMPORTS
+from email.header import Header
 import imaplib
 import email
+from inspect import Attribute
+from operator import methodcaller
 import psycopg2
 import re
 import time
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+from email_reply_parser import EmailReplyParser
 
 
 #####################
@@ -25,13 +29,22 @@ def main():
 ########################
 ### HELPER FUNCTIONS ###
 ########################
+""" 
+FUNCTION NAME: checkMail()
+FUNCTION DESCRIPTION: logs into email server using email username and password provided
+in .env file, then calls helper functions to parse emails and put them into DB
+INPUT: N/A
+OUTPUT: N/A
+"""
+
+
 def checkMail():
-    # ACC CREDENTIALS
+    # ACC CREDENTIALS FROM .ENV FILE
     username = os.getenv("EMAIL_USERNAME")
     password = os.getenv("EMAIL_KEY")
 
     # CREATE AN IMAP4 SERVER
-    gmail_host= 'imap.gmail.com'
+    gmail_host = "imap.gmail.com"
 
     # SET CONNECTION
     mail = imaplib.IMAP4_SSL(gmail_host)
@@ -43,74 +56,144 @@ def checkMail():
     mail.select("INBOX")
 
     # SELECT SPECIFIC EMAILS
-    _, selected_mails = mail.search(None, 'UNSEEN')
+    _, selected_mails = mail.search(None, "UNSEEN")
 
     # TOTAL NUMBER OF UNSEEN MAILS
-    print("Total Unread Messages:" , len(selected_mails[0].split()))
+    print("Total Unread Messages:", len(selected_mails[0].split()))
 
     for num in selected_mails[0].split():
-        _, data = mail.fetch(num , '(RFC822)')
+        _, data = mail.fetch(num, "(RFC822)")
         _, bytes_data = data[0]
 
         # CONVERT THE BYTE INTO MESSAGE
         email_message = email.message_from_bytes(bytes_data)
-        
 
-        values = formatEmail(email_message)
-        
-        
-        connection = psycopg2.connect(host='localhost',
-                                        database='teambanditdb',
-                                        user='quinnmelssen',
-                                        password='ZeroTwo02!')
+        # CHECK TO SEE IF EMAIL IS FWDED
+        if "Fwd:" in email_message["subject"]:
+            values = formatForwardedEmail(email_message)
+        else:
+            values = formatEmail(email_message)
 
-        # SET UP CURSOR
-        cur = connection.cursor()
-    
-        # CREATE QUERY STATEMENT
-        query = "INSERT INTO messages (recipient, sender, subject, message, datetime) VALUES (%s, %s, %s, %s, %s)"
-
-        cur.execute(query, values)
-        
-        connection.commit()
+        executeQuery(values)
 
 
-def executeQuery():
-    """ TAKES IN VALUES GARNERED FROM OTHER FUNCTIONS AND
-    EXECUTES QUERY TO INSERT INTO DATABASE """
+""" 
+FUNCTION NAME: executeQuery()
+FUNCTION DESCRIPTION: Connects to DB, established cursor, and executes insertion query loaded with
+values passed in from caller
+INPUT: Values to be input into messages table
+OUTPUT: N/A
+"""
+
+
+def executeQuery(values):
+    connection = psycopg2.connect(
+        host="localhost",
+        database="teambanditdb",
+        user="quinnmelssen",
+        password="ZeroTwo02!",
+    )
+
+    # SET UP CURSOR
+    cur = connection.cursor()
+
+    print(values)
+    # CREATE QUERY STATEMENT
+    query = "INSERT INTO messages (recipient, sender, subject, message, datetime) VALUES (%s, %s, %s, %s, %s)"
+
+    # EXECUTE STRING QUERY WITH VALUES PASSED INTO FUNCTION
+    cur.execute(query, values)
+
+    # COMMIT ALL CHANGES
+    connection.commit()
+
+
+""" 
+FUNCTION NAME: formatEmail()
+FUNCTION DESCRIPTION: Formats email messages passed in by caller in order to get plaintext strings
+for database insertion
+INPUT: email_message in encoded format
+OUTPUT: Values extracted from email (recipient, sender, subject, message, time)
+"""
+
 
 def formatEmail(email_message):
     # FORMAT EMAIL ADDRESSES IF FIRST/NAME INCLUDED
-        if '<' in email_message["to"]:
-                # PATTERN TO SEARCH FOR
-                pattern = "\<(.+?)\>"
-                # SET RECIPITENT
-                recipient = re.search(pattern, email_message["to"]).group(1)
+    if "<" in email_message["to"]:
+        # PATTERN TO SEARCH FOR
+        pattern = "\<(.+?)\>"
+        # SET RECIPITENT
+        recipient = re.search(pattern, email_message["to"]).group(1)
 
-        if '<' in email_message["from"]:
-                # PATTERN TO SEARCH FOR
-                pattern = "\<(.+?)\>"
-                # SET SENDER
-                sender = re.search(pattern, email_message["from"]).group(1)
+    if "<" in email_message["from"]:
+        # PATTERN TO SEARCH FOR
+        pattern = "\<(.+?)\>"
+        # SET SENDER
+        sender = re.search(pattern, email_message["from"]).group(1)
 
-        # ACCESS MESSSAGE
-        for part in email_message.walk():
-            if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
-                message = part.get_payload(decode=True)
-                break
+    # ACCESS MESSSAGE
+    for part in email_message.walk():
+        if (
+            part.get_content_type() == "text/plain"
+            or part.get_content_type() == "text/html"
+        ):
+            message = part.get_payload(decode=True).decode()
+            break
 
-        # GET TIME IN CORRECT FORMAT
-        time = email_message["date"]
-        pattern = "\, (.+?)\ -"
-        time = re.search(pattern, time).group(1)
-        # FORMAT TIME
-        time = datetime.strptime(time, "%d %b %Y %H:%M:%S")
+    # FORMAT TIME
+    time = email_message["date"]
+    # CHECK FOR TIMEZONES
+    if "-" in time:
+        time = time.split(" -", 1)
+        time = time[0]
 
-        # GET SUBJECT
-        subject = email_message["subject"]
+    elif "+" in time:
+        time = time.split(" +", 1)
+        time = time[0]
 
-        # RETURN VALUES FOR QUERY
-        return (recipient, sender, subject, message, time)
+    time = datetime.strptime(time, "%a, %d %b %Y %H:%M:%S")
 
-if __name__ == '__main__':
+    # GET SUBJECT
+    subject = email_message["subject"]
+    # PARSE OUT RE: FROM SUBJECT IN REPLY EMAILS
+    subject = subject.replace("Re: ", "")
+
+    # CHECK MESSAGE TO SEE IF ITS A REPLY
+    message = EmailReplyParser.parse_reply(message)
+
+    # RETURN VALUES FOR QUERY
+    return [recipient, sender, subject, message, time]
+
+
+""" FORWARDED EMAILS HAVE A DIFFERENT FORMAT THAN REGULAR EMAILS,
+AND AS SUCH MUST BE PARSED DUE TO META INFORMATION (SENDER, RECIPIENT, ETC)
+BEING IN MESSAGE BODY AND NOT HEADER"""
+
+
+def formatForwardedEmail(email_message):
+    # ACCESS MESSSAGE
+    for part in email_message.walk():
+        if (
+            part.get_content_type() == "text/plain"
+            or part.get_content_type() == "text/html"
+        ):
+            message = part.get_payload(decode=True).decode()
+            break
+
+    # PARSE MESSAGE BODY FOR EMAIL META INFORMATION
+    # SPLIT EMAIL BODY BY LINE
+    messageLines = message.split("\n", 5)
+    
+    # PARSE SENDER FROM SECOND LINE
+    s = 
+    pattern = "AUG\|(.*?)\|UGA"
+
+    substring = re.search(pattern, s).group(1)
+    print(substring)
+
+
+def regexParser():
+
+
+if __name__ == "__main__":
     main()
