9f47a32276e297ec6ca71c7f9cb43d3d57dc89a5
Quinn Melssen <qjm7@nau.edu>
2022-02-18 18:06:32 -0700

    Finishing changes to emailhub (router changes etc)

--- website/server/routes/TeamBanditPages/emailRoutes.js
+++ website/server/routes/TeamBanditPages/emailRoutes.js
@@ -1,28 +1,37 @@
 const router = require("express").Router();
 const pool = require("../../db");
-const authorization = require('../../middleware/authorization');
+const authorization = require("../../middleware/authorization");
 
 // EMAIL ROUTES //
-
-// TODO: PULLS ALL MESSAGES
-router.get("/", authorization, async(req, res) => {
+router.get("/", authorization, async (req, res) => {
     try {
-        console.log(req)
         const user = await pool.query(
-            "SELECT sender FROM messages"
+            "SELECT client_name, client_email FROM clients WHERE clients.organizer_id = $1",
+            [req.user]
         );
 
         res.json(user.rows);
-
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server Error");
     }
 });
 
+router.get("/emails", authorization, async (req, res) => {
+    try {
+        console.log(req);
+        const user = await pool.query(
+            "SELECT message FROM messages WHERE (RECIPIENT = $1 OR SENDER = $1) AND (RECIPIENT = $2 OR SENDER = $2)",
+            [req.user]
+        );
 
-
-
+        console.log(user.rows);
+        res.json(user.rows);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
 // END COURSE ROUTES //
 
 module.exports = router;
