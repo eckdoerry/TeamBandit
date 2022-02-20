196e8f5ea27a70b74eee11bbbbabc650a381580b
Quinn Melssen <qjm7@nau.edu>
2022-02-20 11:20:15 -0700

    Cleaning code

--- website/src/Pages/TeamBandit/OrganizerPages/TeamBanditPages/Clients/Clients.js
+++ website/src/Pages/TeamBandit/OrganizerPages/TeamBanditPages/Clients/Clients.js
@@ -1,122 +1,136 @@
-import { React, useState, useEffect } from 'react';
-import { DataGrid,
-  GridToolbarContainer,
-  GridToolbarColumnsButton,
-  GridToolbarFilterButton,
-  GridToolbarExport,
-} from '@mui/x-data-grid';
-import AddClient from './Components/AddClient';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+import { React, useState, useEffect } from "react";
+import {
+    DataGrid,
+    GridToolbarContainer,
+    GridToolbarColumnsButton,
+    GridToolbarFilterButton,
+    GridToolbarExport,
+} from "@mui/x-data-grid";
+import AddClient from "./Components/AddClient";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
 import EditClient from "./Components/EditClient";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 const Clients = () => {
-
     const [clients, setClients] = useState([]);
     const [clientsChange, setClientsChange] = useState(false);
 
     const editButton = (params) => {
-      return (
-        <EditClient client={params.row} setClientsChange={setClientsChange}/>
-      )
+        return (
+            <EditClient
+                client={params.row}
+                setClientsChange={setClientsChange}
+            />
+        );
     };
 
     const deleteButton = (params) => {
-      return (
-          <strong>
-              <Button variant="outlined" color="error" onClick = {() => deleteClient(params.row.client_id)} startIcon={<DeleteIcon />}> Delete </Button>
-          </strong>
-      )
+        return (
+            <strong>
+                <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => deleteClient(params.row.client_id)}
+                    startIcon={<DeleteIcon />}
+                >
+                    {" "}
+                    Delete{" "}
+                </Button>
+            </strong>
+        );
     };
 
-    const getClients = async () =>
-    {
+    const getClients = async () => {
         try {
-            const response = await fetch(`${process.env.REACT_APP_BASEURL}/clients/`, {method: "GET", headers: {token: localStorage.token}});
+            const response = await fetch(
+                `${process.env.REACT_APP_BASEURL}/clients/`,
+                { method: "GET", headers: { token: localStorage.token } }
+            );
 
             const parseData = await response.json();
             setClients(parseData);
-
         } catch (error) {
             console.error(error.message);
         }
-    }
+    };
 
     const deleteClient = async (id) => {
-      try {
+        try {
+            const response = await fetch(
+                `${process.env.REACT_APP_BASEURL}/clients/deleteclient/${id}`,
+                {
+                    method: "DELETE",
+                    headers: { token: localStorage.token },
+                }
+            );
 
-        const response = await fetch(`${process.env.REACT_APP_BASEURL}/clients/deleteclient/${id}`, {
-              method: "DELETE",
-              headers: { token: localStorage.token }
-          });
+            const resp = await response.json();
 
-          const resp = await response.json();
-
-          toast.success(resp);
-          setClientsChange(true);
-      } catch (error) {
-          console.error(error.message);
-          toast.error("Failed to delete client!");
-      }
-    }
+            toast.success(resp);
+            setClientsChange(true);
+        } catch (error) {
+            console.error(error.message);
+            toast.error("Failed to delete client!");
+        }
+    };
 
     const clientFields = [
-      {
-        field: 'client_name',
-        headerName: 'Client Name',
-        flex: 1
-      },
-      {
-        field: 'client_email',
-        headerName: 'Email',
-        flex: 1
-      },
-      {
-        field: 'client_company',
-        headerName: 'Company',
-        flex: 1
-      },
-      {
-        field: 'project',
-        headerName: 'Project',
-        flex: 1
-      },
-      {
-        field: 'client_notes',
-        headerName: 'Notes',
-        flex: 1
-      },
-      {
-        field: 'edit',
-        headerName: '',
-        sortable: false,
-        filterable: false,
-        flex: 1,
-        renderCell: editButton,
-        disableClickEventBubbling: true
-    },
-    {
-        field: 'delete',
-        headerName: '',
-        sortable: false,
-        filterable: false,
-        flex: 1,
-        renderCell: deleteButton,
-        disableClickEventBubbling: true
-    }
+        {
+            field: "client_name",
+            headerName: "Client Name",
+            flex: 1,
+        },
+        {
+            field: "client_email",
+            headerName: "Email",
+            flex: 1,
+        },
+        {
+            field: "client_company",
+            headerName: "Company",
+            flex: 1,
+        },
+        {
+            field: "project",
+            headerName: "Project",
+            flex: 1,
+        },
+        {
+            field: "client_notes",
+            headerName: "Notes",
+            flex: 1,
+        },
+        {
+            field: "edit",
+            headerName: "",
+            sortable: false,
+            filterable: false,
+            flex: 1,
+            renderCell: editButton,
+            disableClickEventBubbling: true,
+        },
+        {
+            field: "delete",
+            headerName: "",
+            sortable: false,
+            filterable: false,
+            flex: 1,
+            renderCell: deleteButton,
+            disableClickEventBubbling: true,
+        },
     ];
 
     const CustomToolbar = () => {
-      return (
-      <GridToolbarContainer>
-          <GridToolbarColumnsButton  sx={{ m: 1 }} />
-          <GridToolbarFilterButton sx={{ m: 1 }} />
-          <GridToolbarExport sx={{ m: 1 }} />
-      </GridToolbarContainer>
-      );
-    }
+        return (
+            <GridToolbarContainer>
+                <GridToolbarColumnsButton sx={{ m: 1 }} />
+                <GridToolbarFilterButton sx={{ m: 1 }} />
+                <GridToolbarExport sx={{ m: 1 }} />
+            </GridToolbarContainer>
+        );
+    };
 
     // Updates Page
     useEffect(() => {
@@ -125,27 +139,26 @@ const Clients = () => {
     }, [clientsChange]);
 
     return (
-        <div style={{ height: 400, width: '100%' }}>
+        <div style={{ height: 400, width: "100%" }}>
+            <div className="PageHeader">
+                <h1>Clients</h1>
 
-          <div className='PageHeader'>
-          <h1>Clients</h1>
-
-          <AddClient setClientsChange = {setClientsChange}/>
-          </div>
+                <AddClient setClientsChange={setClientsChange} />
+            </div>
 
             <br />
 
             <DataGrid
-                rows = {clients}
-                columns = {clientFields}
+                rows={clients}
+                columns={clientFields}
                 getRowId={(row) => row.client_id}
-                pageSize = {5}
-                rowsPerPageOptions = {[5]}
-                components = {{Toolbar: CustomToolbar,}}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                components={{ Toolbar: CustomToolbar }}
                 disableSelectionOnClick
             />
         </div>
-  );
-}
+    );
+};
 
 export default Clients;
--- website/src/Pages/TeamBandit/OrganizerPages/TeamBanditPages/Clients/Components/AddClient.js
+++ website/src/Pages/TeamBandit/OrganizerPages/TeamBanditPages/Clients/Components/AddClient.js
@@ -1,141 +1,147 @@
-import { useState, React } from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import { useState, React } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-const AddClient = ({setClientsChange}) => {
-  const [open, setOpen] = useState(false);
-  const [clientName, setClientName] = useState("");
-  const [company, setCompany] = useState("");
-  const [email, setEmail] = useState("");
-  const [notes, setNotes] = useState("");
+const AddClient = ({ setClientsChange }) => {
+    const [open, setOpen] = useState(false);
+    const [clientName, setClientName] = useState("");
+    const [company, setCompany] = useState("");
+    const [email, setEmail] = useState("");
+    const [notes, setNotes] = useState("");
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-  const handleClose = (event) => {
-    setOpen(false);
-  };
+    const handleClose = (event) => {
+        setOpen(false);
+    };
 
-  const onSubmitForm = async e => {
-    e.preventDefault();
-    if (!clientName || !company || !email){
-      alert("Please fill out all required fields");
-      return;
-    }
-    try {
-        const myHeaders = new Headers();
+    const onSubmitForm = async (e) => {
+        e.preventDefault();
+        if (!clientName || !company || !email) {
+            alert("Please fill out all required fields");
+            return;
+        }
+        try {
+            const myHeaders = new Headers();
 
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("token", localStorage.token);
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("token", localStorage.token);
 
-        const body = { clientName, email, company, notes };
-        const response = await fetch(`${process.env.REACT_APP_BASEURL}/clients/addclient`, {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify(body)
-        });
+            const body = { clientName, email, company, notes };
+            const response = await fetch(
+                `${process.env.REACT_APP_BASEURL}/clients/addclient`,
+                {
+                    method: "POST",
+                    headers: myHeaders,
+                    body: JSON.stringify(body),
+                }
+            );
 
-        //const parseResponse = await response.json();
+            //const parseResponse = await response.json();
 
-        toast.success("Client added successfully!");
+            toast.success("Client added successfully!");
 
-        setClientsChange(true);
-        setClientName("");
-        setCompany("");
-        setEmail("");
-        setNotes("");
-    } catch (err) {
-      console.error(err.message);
-      toast.error("Failed to add client!");
-    }
-    handleClose();
-  };
+            setClientsChange(true);
+            setClientName("");
+            setCompany("");
+            setEmail("");
+            setNotes("");
+        } catch (err) {
+            console.error(err.message);
+            toast.error("Failed to add client!");
+        }
+        handleClose();
+    };
 
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Add Client
-      </Button>
+    return (
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Add Client
+            </Button>
 
-      <Dialog open={open} onClose={handleClose}>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Add a New Client</DialogTitle>
 
-        <DialogTitle>Add a New Client</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Enter client information here
+                    </DialogContentText>
 
-        <DialogContent>
+                    <TextField
+                        required
+                        autoFocus
+                        margin="dense"
+                        label="Client Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={clientName}
+                        error={clientName === ""}
+                        helperText={
+                            clientName === "" ? "Client name is required" : " "
+                        }
+                        onChange={(e) => setClientName(e.target.value)}
+                    />
 
-          <DialogContentText>
-            Enter client information here
-          </DialogContentText>
+                    <TextField
+                        required
+                        margin="dense"
+                        label="Company/Location"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={company}
+                        error={company === ""}
+                        helperText={
+                            company === ""
+                                ? "Client company/location is required"
+                                : " "
+                        }
+                        onChange={(e) => setCompany(e.target.value)}
+                    />
 
-          <TextField
-            required
-            autoFocus
-            margin="dense"
-            label="Client Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            value = {clientName}
-            error={clientName === ""}
-            helperText={clientName === "" ? 'Client name is required' : ' '}
-            onChange={(e) => setClientName(e.target.value)}
-          />
+                    <TextField
+                        type="email"
+                        margin="dense"
+                        label="Client Email"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={email}
+                        error={email === ""}
+                        helperText={
+                            email === "" ? "Client email is required" : " "
+                        }
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
 
-          <TextField
-            required
-            margin="dense"
-            label="Company/Location"
-            type="text"
-            fullWidth
-            variant="standard"
-            value = {company}
-            error={company === ""}
-            helperText={company === "" ? 'Client company/location is required' : ' '}
-            onChange={(e) => setCompany(e.target.value)}
-          />
+                    <TextField
+                        margin="dense"
+                        label="Notes"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                        value={notes}
+                        onChange={(e) => setNotes(e.target.value)}
+                    />
+                </DialogContent>
 
-          <TextField
-            type="email"
-            margin="dense"
-            label="Client Email"
-            type="text"
-            fullWidth
-            variant="standard"
-            value = {email}
-            error={email === ""}
-            helperText={email === "" ? 'Client email is required' : ' '}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-           <TextField
-            margin="dense"
-            label="Notes"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={notes}
-            onChange={(e) => setNotes(e.target.value)}
-          />
-
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick = {onSubmitForm}>Add Client</Button>
-        </DialogActions>
-
-      </Dialog>
-
-    </div>
-  );
-}
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={onSubmitForm}>Add Client</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+};
 
 export default AddClient;
